ca255bcfe9483f0f5cf455a4eed78296fe27cf22
Arshaluys Aslanyan <a.aslanyan.a@gmail.com>
2020-05-26 12:17:05 +0300

    adding input validation with Vue simple validation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   .container
     todo
+    todo
 </template>
 
 <script>
--- src/components/todoinput.vue
+++ src/components/todoinput.vue
@@ -1,17 +1,26 @@
 <template lang="pug">
     .todo_input
+        div.error {{validation.firstError('todo.name')}}
         input(
             type="text"
             placeholder="My TODO list ..."
             autofocus
             v-model="todo.name"
+            :class="{validError : validation.hasError()}"
             @keydown.enter="addTodo"
             ).input
 </template>
 
 <script>
+import { Validator } from 'simple-vue-validator';
 let uniqID = 1;
 export default {
+    mixins: [require("simple-vue-validator").mixin],
+  validators: {
+    "todo.name"(value) {
+      return Validator.value(value).required('Поле не должно быть пустым');
+    }
+  },
     data() {
         return {
             todo: {
@@ -23,16 +32,27 @@ export default {
     },
     methods: {
         addTodo() {
-            uniqID++;
-            this.$emit('addTodo', {...this.todo});
-            this.todo.name = '';
-            this.todo.id = uniqID;
+            this.$validate().then(success => {
+                if(!success) return;
+
+                uniqID++;
+                this.$emit('addTodo', {...this.todo});
+                this.todo.name = '';
+                this.todo.id = uniqID;
+
+                this.validation.reset();
+            })
+            
         }
     },
 }
 </script>
 
 <style lang="scss" scoped>
+.todo_input {
+    position: relative;
+}
+
 .input {
     font-size: 24px;
     padding: 16px 16px 16px 60px;
@@ -44,5 +64,16 @@ export default {
     color: inherit;
     width: 100%;
     background: #fff;
+}
+
+.validError {
+    border: 1px solid red;
+}
+
+.error {
+    position: absolute;
+    top: -30px;
+    color: red;
+    font-weight: bold;
 }
 </style>
